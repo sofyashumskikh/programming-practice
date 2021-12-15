9965654d7d0c574bcda0b58bd3b3f9dcfa607cae
sofyashumskikh <91876971+sofyashumskikh@users.noreply.github.com>
2021-12-16 00:10:43 +0300

    Update systems.cpp

--- homeworks/C++/03/systems.cpp
+++ homeworks/C++/03/systems.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 int main() {
 
@@ -18,12 +19,18 @@ int main() {
 	std::cout << std::endl;
 	std::cout << "number " << n << " in " << sys << " system = ";
 
+	std::string result = "";
+
 	while (n > 0) {
 		if (n % sys > 9)
-			std::cout << char('A' + (n % sys) - 10);
+			result += char('A' + (n % sys) - 10);
 		else
-			std::cout << n % sys;
+			result += char((n % sys)+48);
 		n /= sys;
+	}
+
+	for (int i = result.length(); i >= 0; --i) {
+		std::cout << result[i];
 	}
 
 	std::cout << std::endl;
